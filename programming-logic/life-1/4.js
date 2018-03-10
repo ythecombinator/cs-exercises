39c463ba95945ddb4c32975bda0bc304c13f4aed
Matheus Brasil <matheus.brasil10@gmail.com>
2018-03-09 23:47:14 -0300

    solution(programming-logic/life-1): question 4

---
+++ programming-logic/life-1/4.js
@@ -0,0 +1,16 @@
+const {askUser, handleError} = require('./helpers');
+
+const questions = [1, 2, 3, 4].map(number => `Quanto você consumiu na sua ${number}° refeição? (Cal)`);
+
+const calcTotalCalories = calories => calories
+	.map(calorie => Number(calorie))
+	.reduce((a, b) => a + b, 0);
+
+const printResult = answers => {
+	const totalCalories = calcTotalCalories(answers);
+  console.log(`\nVocê consumiu um total de ${totalCalories} calorias.\n`);
+};
+
+askUser(questions)
+	.then(printResult)
+	.catch(handleError);
