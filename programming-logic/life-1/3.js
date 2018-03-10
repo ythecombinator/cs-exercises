4d403030ccda74170b1b0832a84eaec564d0c56f
Matheus Brasil <matheus.brasil10@gmail.com>
2018-03-09 23:11:02 -0300

    solution(programming-logic/life-1): question 3

---
+++ programming-logic/life-1/3.js
@@ -0,0 +1,35 @@
+const {askUser, handleError} = require('./helpers');
+
+const questions = ['Qual o montante inicial? (R$)', 'Qual a taxa de juros? (%/mês)'];
+
+const numbers = /[-.0-9]+/;
+
+const parseInitialAmount = userInput => {
+	const [value] = userInput.match(numbers);
+	return Number(value);
+};
+
+const calcSavingsIncome = (initialAmount, duration, rate) => {
+	const results = [];
+
+	for (let i = 0; i < duration; i++) {
+		if (i === 0) {
+      results.push({month: i, amount: initialAmount});
+		}
+
+    results.push({month: i, amount: results[i].amount + (results[i].amount * rate * 0.001)});
+	}
+	return results.slice(1);
+};
+
+const printResult = answers => {
+	const [amount, rate] = answers;
+	const results = calcSavingsIncome(parseInitialAmount(amount), 6, rate);
+  results.forEach(({month, amount}) => {
+    console.log(`\nApós ${month + 1} mes(es), haverá R$${amount.toFixed(2)}.\n`);
+  });
+};
+
+askUser(questions)
+	.then(printResult)
+	.catch(handleError);
